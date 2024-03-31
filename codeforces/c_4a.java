b2672a4b3bd441f4220dad656c6e18b6764ff588
yuto0226 <wayne71112@gmail.com>
2024-03-31 11:18:47 +0800

    AC codeforces c4a
    upload answer code

---
+++ codeforces/c_4a.java
@@ -0,0 +1,19 @@
+// CodeForces 4A
+// AC | 622 ms | 0 KB
+package codeforces;
+import java.util.Scanner;
+
+public class c_4a {
+    public static void main(String[] args) {
+        int n;
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextInt()) {
+            n = s.nextInt();
+            if (n > 2 && n % 2 == 0) {
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
