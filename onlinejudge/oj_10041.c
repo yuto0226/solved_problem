c732e8cc722a900c1f7167543ca90d78985db94a
yuto0226 <wayne71112@gmail.com>
2024-04-12 22:59:25 +0800

    Recover oj_10041.c

--- onlinejudge/oj_10041.c
+++ onlinejudge/oj_10041.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-  unsigned long long f, o;
-  while(scanf("%llu %llu", &f, &o)!=EOF){
-    printf("%llu\n", f > o ? f - o : o - f);
-  }
-  return 0;
+int cmp(const void *a, const void *b) {
+    return *(int *)a - *(int *)b;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    while (n--) {
+        int r;
+        scanf("%d", &r);
+      
+        int street[r];
+        for (int i = 0; i < r; i++) {
+            scanf("%d", &street[i]);
+        }
+        qsort(street, r, sizeof(int), cmp);
+      
+        int sum = 0;
+        for (int i = 0; i < r; i++) {
+            sum += abs(street[r/2] - street[i]);
+        }
+      
+        printf("%d\n", sum);
+    }
 }
