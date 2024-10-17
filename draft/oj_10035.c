2901179cd8bdb3566a51ca767a6ff77500a0dfa6
yuto0226 <wayne71112@gmail.com>
2024-10-17 12:20:51 +0800

    Orginize files

--- draft/oj_10035.c
+++ draft/oj_10035.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main() {
-  int a, b;
+  unsigned int a, b;
   while (scanf("%d %d", &a, &b) != EOF) {
     if (a == 0 && b == 0)
       break;
     int carrys = 0;
-    while (a != 0 && b != 0) {
-      if (a % 10 + b % 10 > 9) {
-        a += 10;
+    int c = 0;
+    while (a != 0 || b != 0) {
+      // printf("%d + %d + %d = %d\n", a % 10, b % 10, c, a % 10 + b % 10);
+      int sum = a % 10 + b % 10 + c;
+      c = 0;
+      if (sum > 9) {
+        c += 1;
         carrys++;
       }
       a /= 10;
